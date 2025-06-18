63e4af7e2280f6e420c501112325642c8e72f4b2
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-06-18 12:48:47 +0530

    finished firebase setup and user sign up

---
+++ src/config/firebase.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { getFirestore, setDoc,doc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDx_Zqab7S7FJ23RsbviiWHFnFdT-4qzBI",
+  authDomain: "chat-app-byd.firebaseapp.com",
+  projectId: "chat-app-byd",
+  storageBucket: "chat-app-byd.firebasestorage.app",
+  messagingSenderId: "769999230388",
+  appId: "1:769999230388:web:7c05d3b986ac546f8f4ee1"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username,email,password)=>{
+try {
+    const res = await createUserWithEmailAndPassword(auth,email, password);
+    const user = res.user;
+    await setDoc(doc(db,"users",user.uid),{
+        id:user.uid,
+        username:username.toLowerCase(),
+        email,
+        name:"",
+        avatar:"",
+        bio:"Hey! there I am using chat app",
+        lastSeen: Date.now()
+
+    })
+    await setDoc(doc(db,"chats",user.uid),{
+        chatData:[]
+    })
+} catch (error) {
+    console.error(error)
+    toast.error(error.code)
+}
+}
+export {signup}
